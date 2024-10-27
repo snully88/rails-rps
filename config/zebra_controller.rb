aa36309828f6a6d325aa8005facc46ec14fe5a4f
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-10-27 02:34:51 +0000

    urrrr

---
+++ config/zebra_controller.rb
@@ -0,0 +1,12 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @random_move ["rock", "paper", "scissors"]
+    render({ :template => "game_templates/play_rock"})
+  end 
+
+  def elephant
+  end
+
+  def lion
+  end
+end
